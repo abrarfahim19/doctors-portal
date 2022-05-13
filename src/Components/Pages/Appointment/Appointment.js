a2323d5ca577d57de23e850533d9d96c1b5a5c15
abrarfahim19 <fahimpatwari19@gmail.com>
2022-05-13 21:00:57 +0600

    Appointment Rebasing

--- src/Components/Pages/Appointment/Appointment.js
+++ src/Components/Pages/Appointment/Appointment.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Appointment = () => {
     return (
         <div>
-            <h2>This is Appointment</h2>
+            <h2>This is the Appointment</h2>
         </div>
     );
 };
